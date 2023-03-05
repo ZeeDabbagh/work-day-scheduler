ab886423b7998cd25050d61b2226ac39841f7ce4
ZeeDabs <dabbagh.zainab@gmail.com>
2023-03-05 12:31:07 -0500

    added event listener but there is a bug that deactivates the class changes based on the hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,8 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var userInput = $('.description');
 
-var description = [];
-var textAreaEl = $('textarea')
-var currentTime = dayjs().format('H')
+
 
 $(function () {
   
@@ -15,11 +12,33 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+  
+  function renderDesc() {
+    var userInput = localStorage.getItem('userInput')
+
+    if (!userInput) {
+      return;
+    }
+
+    $('textarea').textContent = userInput
+  }
+  
+  // var saveBtn = $('.saveBtn');
+  // saveBtn.addEventListener('click', function(event) {
+  //   event.preventDefault();
+
+  //   var userInput = $('.description').value
+  //   localStorage.setItem('userInput', userInput)
+  //   renderDesc();
+  // })
+
 
   
   // Apply the past, present, or future class to each time block 
 
   var timeBlock = document.getElementsByClassName('time-block');
+  var textAreaEl = $('textarea');
+  var currentTime = dayjs().format('H');
 
   for (var i = 0; i < timeBlock.length; i++) {
     if (parseInt(timeBlock[i].id) < currentTime) {
@@ -36,11 +55,16 @@ $(function () {
 
 
   // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+  // the values of the corres
+  
+  // Store data
+  var userInput = localStorage.getItem('userInput');
 
+  if (!userInput) {
+    return;
+  }
 
+  $('textarea').textContent = userInput
 
   // Display the current date in the header of the page.
   var today = dayjs();
